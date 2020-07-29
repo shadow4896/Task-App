4871b6eb2a87faad9a1f85c9f1140a26d5b512da
Shadow4896 <thomac59@my.erau.edu>
2020-07-29 17:25:51 -0500

    Updated app

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            
+            try 
+            {
+                num = 17;
+                Console.WriteLine("Please enter number 17");
+                num = int.Parse(Console.ReadLine());
+                Console.WriteLine(num);
+            }
+            catch 
+            {
+                Console.WriteLine("Done");
+            }
+        }
+    }
+}
---
+++ TaskList/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Threading.Tasks;
+
+namespace TaskList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int selectnum = 0;
+            Console.WriteLine("Welcome to the Console Task Manager List");
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("What would you like to do:");
+            List<string> TaskList = new List<string>();
+
+            Console.WriteLine("1. Create an item. \n2. Action an item\n0. Exit out program");
+            selectnum = int.Parse(Console.ReadLine());
+
+            if (selectnum == 1)
+            {
+                Console.WriteLine("Add a task");
+                string task = "";
+                Console.WriteLine("Please enter a task");
+                task = Console.ReadLine();
+                TaskList.Add(task);
+                
+            }
+            else if (selectnum == 2)
+            {
+                Console.WriteLine("You would like to action an item");
+            }
+            else if (selectnum == 0)
+                Console.WriteLine("Thank you, Good bye!!!);");
+
+
+            Console.WriteLine(TaskList[0]);
+
+        }
+        public static string Task()
+        {
+            string taskList = "";
+            Console.WriteLine("Please enter a task");
+            taskList = Console.ReadLine();
+            return taskList;
+        }
+
+
+    }
+}
